27900de7a6c55a2fc494abe70247b01f2ce31b1d
Jeremy Trimble <devjeremyt@gmail.com>
2020-04-26 06:42:09 -0400

    added buttons for playmethod

--- scripts/fullscreen.js
+++ scripts/fullscreen.js
@@ -1,9 +1,16 @@
 let body = document.querySelector('html');
 let video = document.querySelector('#video');
 
+const clickEvent = new MouseEvent('click', {
+    view:window,
+    bubbles: true,
+    cancelable: true
+});
+
 console.log(body)
 console.log(video);
-body.addEventListener('click', ()=>{
+
+const listener = function(){body.addEventListener('click', ()=>{
     console.log('clicked')
     if(!document.mozFullScreenElement){
         video.requestFullscreen();
@@ -11,5 +18,10 @@ body.addEventListener('click', ()=>{
         video.requestFullscreen();
     }
 });
+}
+listener()
+window.onload = body.dispatchEvent(clickEvent);
+
+
 
 
